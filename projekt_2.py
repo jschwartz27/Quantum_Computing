c6f636a80eb4536c5710e37f5a3f348ebaf3a82b
Joshua E. Schwartz <josh.schwartz92@gmail.com>
2018-10-17 22:10:36 +0200

    Update projekt_2.py

--- projekt_2.py
+++ projekt_2.py
@@ -10,9 +10,6 @@ ax = fig.add_subplot(111, projection='3d')
 z_lim = 40
 number = 50000
 n = np.arange(0, number, 1)
-m = np.arange(0, 100, 1)
-z = 2 * n
-z2 = 4 * n
 
 def label(equation):
    label = 'O' + str(equation)
@@ -22,16 +19,15 @@ def ratio(objekt):
    eqs = list(objekt.values())[0]
    ratio = eqs[0]/eqs[1]
    return ratio
-a = n 
 
 # list is [Classical, Quantum]
-algorithms = [{"Shor's Algorithm": [(np.exp(np.log(a) ** (1/3) * (np.log(np.log(a))) ** (2/3)) ** (1/3)), (np.log(a) ** 3)]},
-              {"Testing Group Commutivity": [(a), (a**(2/3) * np.log(a))]},
-              {"Element Distinctions": [(a), (a**(2/3))]},
-              {"Matrix Product Verification": [(a**2), (a**(5/3))]},
-              {"Grover's Algorithm": [(a), (np.sqrt(a))]},
-              {"Triangle in Graph": [(a**2), (a**1.25)]},
-              {"Quantum Fourier Transformation": [(np.log(a) * a), (np.log(a))]}  
+algorithms = [{"Shor's Algorithm": [(np.exp(np.log(n) ** (1/3) * (np.log(np.log(n))) ** (2/3)) ** (1/3)), (np.log(n) ** 3)]},
+              {"Testing Group Commutivity": [(n), (n**(2/3) * np.log(n))]},
+              {"Element Distinctions": [(n), (n**(2/3))]},
+              {"Matrix Product Verification": [(n**2), (n**(5/3))]},
+              {"Grover's Algorithm": [(n), (np.sqrt(n))]},
+              {"Triangle in Graph": [(n**2), (n**1.25)]},
+              {"Quantum Fourier Transformation": [(np.log(n) * n), (np.log(n))]}  
               ]
 algorithms.reverse()
 # Plot complexities
